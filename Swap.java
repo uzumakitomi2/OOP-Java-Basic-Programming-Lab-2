2381c94673ae69563bea1c062599c3a4b07df396
Rubina Amin <aunamikarubina@gmail.com>
2026-08-13 12:23:40 +0600

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main (String[] args){
+    int a = 5;
+    int b = 8;
+    System.out.println("Before swap:");
+    System.out.println("a =" + a);
+    System.out.println("b =" + b);
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("After swap:");
+    System.out.println("a ="+ a);
+    System.out.println("b ="+ b);    
+}
+}
