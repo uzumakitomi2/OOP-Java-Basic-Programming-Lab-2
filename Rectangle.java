ddcea1b7ca8aecf210bf92cc47083539d9a14b3f
Rubina Amin <aunamikarubina@gmail.com>
2026-08-13 12:22:14 +0600

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String[] args){
+        double width = 5.5;
+        double height = 8.5;
+        double perimeter =2*(width+height);
+        double area = height*width;
+        System.out.println("Perimeter =" + perimeter);
+        System.out.println("Area =" + area );
+    }
+    
+}
