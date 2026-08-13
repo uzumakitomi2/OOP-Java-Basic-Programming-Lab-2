40319571e02c0ef292ff33ac7045b6ae00972df2
Rubina Amin <aunamikarubina@gmail.com>
2026-08-13 12:13:19 +0600

    Create Expression Evlauation.java

---
+++ Expression Evlauation.java
@@ -0,0 +1,7 @@
+public class Evaluation {
+    public static void main(String[] args){
+        double result = ((25.5*3.5-3.5*3.5)/(40.5-4.5));
+        System.out.println("Result = " + result);
+    }
+    
+}
