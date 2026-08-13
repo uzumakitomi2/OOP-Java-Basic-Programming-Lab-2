ab85eefd793f8858b5e751e0f82cbee593cfd3e2
Rubina Amin <aunamikarubina@gmail.com>
2026-08-13 12:20:24 +0600

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle {
+    public static void main(String[] args){
+        double radius = 7.5;
+        double perimeter = 2*Math.PI*radius;
+        double area = Math.PI*radius*radius;
+        System.out.println("Perimeter = "+ perimeter);
+        System.out.println("Area = " + area);
+    }
+    
+}
